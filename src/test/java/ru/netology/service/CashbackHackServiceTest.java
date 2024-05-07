350a94164419ae012e7a6ae82a37fe32fbe4e371
Liubov B <l.boukreeva.qa@gmail.com>
2024-05-07 16:27:17 +0300

    2 commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testBoundaryAmount999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testBoundaryAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+}
